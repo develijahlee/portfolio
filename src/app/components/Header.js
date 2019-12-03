ba9e52b0750fe0c4ea9cdab0d8484c9a5043346c
develijahlee <54133475+develijahlee@users.noreply.github.com>
2019-12-03 22:13:50 +0900

    Nav animation
    updated Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Icon from "./Icon.js";
 
 // Import CSS
 import "../styles/components/header.css";
@@ -12,76 +13,76 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="container-fluid">
+          <Link to="/">
+            <Icon />
+            <span>Lee</span>
+          </Link>
           <ul>
-              <li className="nav-languages">
-                  <a
-                    className={
-                      context.state.language === "en" ? "link-active" : "link"
-                    }
-                    onClick={context.changeLanguage}
-                    data-language="en"
-                  >
-                    ENG
-                  </a>
-
-              </li>
-              <li className="nav-languages">
-                  <a
-                    className={
-                      context.state.language === "kr" ? "link-active" : "link"
-                    }
-                    onClick={context.changeLanguage}
-                    data-language="kr"
-                  >
-                    한국어
-                  </a>
-              </li>
+            <li className="nav-languages">
+              <Link
+                className={
+                  context.state.language === "en" ? "link-active" : "link"
+                }
+                onClick={context.changeLanguage}
+                data-language="en"
+                href="/#"
+              >
+                ENG
+              </Link>
+            </li>
+            <li className="nav-languages">
+              <Link
+                className={
+                  context.state.language === "kr" ? "link-active" : "link"
+                }
+                onClick={context.changeLanguage}
+                data-language="kr"
+                href="/#"
+              >
+                한국어
+              </Link>
+            </li>
           </ul>
           <nav className="nav-wrapper">
-            {context.state.isNavOpen && (
-              <div className="js-nav nav">
-              
-                <ul className="nav-list">
-                  <li>
-                    <Link className="link" to="/" onClick={context.toggleNav}>
-                      {context.state.data.nav.home}
-                    </Link>
-                  </li>
+            <div id="jsNav" className="js-nav nav">
+              <ul className="nav-list">
+                <li>
+                  <Link className="link" to="/" onClick={context.toggleNav}>
+                    {context.state.data.nav.home}
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link
-                      className="link"
-                      to="/about"
-                      onClick={context.toggleNav}
-                    >
-                      {context.state.data.nav.about}
-                    </Link>
-                  </li>
+                <li>
+                  <Link
+                    className="link"
+                    to="/about"
+                    onClick={context.toggleNav}
+                  >
+                    {context.state.data.nav.about}
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link
-                      className="link"
-                      to="/testimonials"
-                      onClick={context.toggleNav}
-                    >
-                      {context.state.data.nav.testimonials}
-                    </Link>
-                  </li>
-
-                </ul>
-              </div>
-            )}
+                <li>
+                  <Link
+                    className="link"
+                    to="/testimonials"
+                    onClick={context.toggleNav}
+                  >
+                    {context.state.data.nav.testimonials}
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
             {
               <button
-                className={
-                  context.state.isNavOpen
-                    ? "nav-toggler nav-toggler--open"
-                    : "nav-toggler"
-                }
+                id="btn"
                 type="button"
                 aria-label="Toggle navigation"
-                onClick={context.toggleNav}
+                className={context.toggleNav === "kr" ? "link-active" : "link"}
+                onClick={function() {
+                  document.getElementById("jsNav").classList.toggle("active");
+                }}
               >
                 menu
               </button>
---
+++ src/app/components/Icon.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="70"
+      height="71"
+      fill="none"
+      viewBox="0 0 70 71"
+    >
+      <path
+        stroke="#fff"
+        strokeWidth="8"
+        d="M66 35.5C66 52.95 52.068 67 35 67 17.933 67 4 52.95 4 35.5S17.933 4 35 4c17.068 0 31 14.05 31 31.5z"
+      />
+      <path
+        fill="#fff"
+        d="M44.105 38.135H31.448v8.57h14.854V52H24.856V20.008h21.402v5.34h-14.81v7.624h12.657v5.163z"
+      />
+    </svg>
+  );
+}
+
+export default Icon;
