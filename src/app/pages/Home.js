7cb19612732a8ec201a9a575bd0453819528818c
develijahlee <54133475+develijahlee@users.noreply.github.com>
2019-12-16 23:03:26 +0900

    Home Introduction
    Made code status responsive

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
     <MyContext.Consumer>
       {context => (
         <main>
-          <div className="container">
-            <div className="container__hero">
+          <div className="container container--home">
+            <div className="intro intro--home">
               <h1>{context.state.data.home.h1}</h1>
 
               <p>{context.state.data.home.p1}</p>
@@ -20,15 +20,15 @@ const Home = () => {
                 {context.state.data.home.button}
               </button>
             </div>
-          </div>
-          <div className="container">
-            <code className="status">
-              <p>{"{"}</p>
-              <p className="indent">"name": Elijah Lee,</p>
-              <p className="indent">"occupation": frontend developer,</p>
-              <p className="indent">"for_hire": true</p>
-              <p>{"}"}</p>
-            </code>
+            <div className="code">
+              <code className="status">
+                <p>{"{"}</p>
+                <p className="indent">"name": Elijah Lee,</p>
+                <p className="indent">"occupation": frontend developer,</p>
+                <p className="indent">"for_hire": true</p>
+                <p>{"}"}</p>
+              </code>
+            </div>
           </div>
         </main>
       )}
