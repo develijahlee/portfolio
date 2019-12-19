a9b1f4168c80ade1deae3c8b72120d62cb353dca
develijahlee <54133475+develijahlee@users.noreply.github.com>
2019-12-19 23:15:24 +0900

    Contact
    added contact section

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -14,49 +14,73 @@ const Home = () => {
     <MyContext.Consumer>
       {context => (
         <main>
-          <section className="container container--home">
-            <div className="intro intro--home">
-              <h1>{context.state.data.home.h1}</h1>
+          <section>
+            <div className="container container--home">
+              <div className="intro intro--home">
+                <h1>{context.state.data.home.name}</h1>
 
-              <p>{context.state.data.home.p1}</p>
-              <p>{context.state.data.home.p2}</p>
-              <button className="btn btn--cv">
-                {context.state.data.home.button}
-              </button>
-            </div>
-            <div className="code">
-              <code className="status">
-                <p>{"{"}</p>
-                <p className="indent">"name": Elijah Lee,</p>
-                <p className="indent">"occupation": frontend developer,</p>
-                <p className="indent">"for_hire": true</p>
-                <p>{"}"}</p>
-              </code>
+                <p>{context.state.data.home.p1}</p>
+                <p>{context.state.data.home.p2}</p>
+                <button className="btn btn--cv">
+                  {context.state.data.home.cv}
+                </button>
+              </div>
+              <div className="code">
+                <code className="status">
+                  <p>{"{"}</p>
+                  <p className="indent">"name": Elijah Lee,</p>
+                  <p className="indent">"occupation": frontend developer,</p>
+                  <p className="indent">"for_hire": true</p>
+                  <p>{"}"}</p>
+                </code>
+              </div>
             </div>
           </section>
-          <section className="projects">
-            <ul className="projects__list">
-              <li>
-                <a
-                  className="yonsei"
-                  href="https://develijahlee.github.io/uic/"
-                  target="_blank"
-                >
-                  <img src={yonsei} alt="Yonsei University Logo" />
-                </a>
-                <h3>Underwood International College</h3>
-              </li>
-              <li>
-                <a
-                  className="scribe"
-                  href="https://yonseiuicscribe.com"
-                  target="_blank"
-                >
-                  <img src={scribe} alt="UIC Scribe Logo" />
-                </a>
-                <h3>UIC Scribe</h3>
-              </li>
-            </ul>
+          <section>
+            <div className="projects">
+              <ul className="projects__list">
+                <li>
+                  <a
+                    className="yonsei"
+                    href="https://develijahlee.github.io/uic/"
+                    target="_blank"
+                  >
+                    <img src={yonsei} alt="Yonsei University Logo" />
+                  </a>
+                  <h3>Underwood International College</h3>
+                </li>
+                <li>
+                  <a
+                    className="scribe"
+                    href="https://yonseiuicscribe.com"
+                    target="_blank"
+                  >
+                    <img src={scribe} alt="UIC Scribe Logo" />
+                  </a>
+                  <h3>UIC Scribe</h3>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section className="contact">
+            <div className="container container--contact">
+              <h1>{context.state.data.home.contact}</h1>
+              <form action="https://formspree.io/you@email.com">
+                <label for="name">{context.state.data.home.form_name}</label>
+                <input type="text" name="name" />
+                <label for="email">{context.state.data.home.form_email}</label>
+                <input type="email" name="_replyto" />
+                <label for="message">
+                  {context.state.data.home.form_message}
+                </label>
+                <textarea name="message" cols="30" rows="10" />
+                <input
+                  type="submit"
+                  value={context.state.data.home.form_button}
+                  className="btn btn--form"
+                />
+              </form>
+            </div>
           </section>
         </main>
       )}
