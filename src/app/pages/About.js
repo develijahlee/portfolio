12726d7f7916dabc63c57f584f9c434392171e18
develijahlee <54133475+develijahlee@users.noreply.github.com>
2019-12-28 23:01:08 +0900

    About Content
    wrote eng and korean content of methodology and tools

--- src/app/pages/About.js
+++ src/app/pages/About.js
@@ -18,20 +18,8 @@ const About = () => {
           </section>
           <section className="history">
             <div className="container container--about">
-              <p>
-                Born in South Korea and raised in the United States for 13
-                years, I discovered my passion for website development in
-                freshman year of college in 2015. What began as a hobby from
-                making websites for college clubs became a passionate career
-                interest that persisted for years as I found again and again
-                that I loved coding and desgning websites.
-              </p>
-              <p>
-                What I enjoy about my work is that there is always something to
-                improve on. The field of website development, like all internet
-                technologies, is relatively young and is continuously innovating
-                to provide better experiences for users.
-              </p>
+              <p>{context.state.data.about.methodology}</p>
+              <p>{context.state.data.about.tools}</p>
             </div>
           </section>
         </main>
