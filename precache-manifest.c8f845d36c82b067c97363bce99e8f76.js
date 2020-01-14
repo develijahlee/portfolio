e86fe1eb7084ddab7447dafaa3a20c3673d54863
develijahlee <54133475+develijahlee@users.noreply.github.com>
2020-01-14 21:59:20 +0900

    Updates

---
+++ precache-manifest.c8f845d36c82b067c97363bce99e8f76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f499f8b085aaa8a2e6db89fb6d9f69c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf86fb1126b821d72764",
+    "url": "/portfolio/static/css/main.6a81289f.chunk.css"
+  },
+  {
+    "revision": "52a9460c3b6a99118bd1",
+    "url": "/portfolio/static/js/2.3e13c774.chunk.js"
+  },
+  {
+    "revision": "cf86fb1126b821d72764",
+    "url": "/portfolio/static/js/main.fb018f8a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6e7596264570b2bbf205f5ccc788259b",
+    "url": "/portfolio/static/media/scribe-logo.6e759626.png"
+  },
+  {
+    "revision": "5f57b74f200fba3fd3109b12f0e44e6e",
+    "url": "/portfolio/static/media/yonsei-logo.5f57b74f.png"
+  }
+]);
