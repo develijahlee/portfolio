6543841870fa4d02b860fc2b6df9fa600d6952a1
Elijah <develiahlee@gmail.com>
2020-02-04 18:52:51 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d013540352522ecc053dd271753d5037.js"
+  "/portfolio/precache-manifest.ef936e552b4a778bf16860d87fb13dad.js"
 );
 
 self.addEventListener('message', (event) => {
