feca31d40618cbfe450018c2b949ebb3146c5b81
Elijah <develiahlee@gmail.com>
2020-02-23 19:57:46 +0900

    commit changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import { HashRouter } from "react-router-dom";
-=======
-import { BrowserRouter } from "react-router-dom";
->>>>>>> bf686fcb6e918e8e35b42ff103b6d15d6e20e271
 
 // Import service workers
 import registerServiceWorker from "./registerServiceWorker";
@@ -24,15 +20,9 @@ import "./app/sass/index.scss";
 const App = () => {
   return (
     <MyProvider>
-<<<<<<< HEAD
       <HashRouter>
         <Main />
       </HashRouter>
-=======
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
->>>>>>> bf686fcb6e918e8e35b42ff103b6d15d6e20e271
     </MyProvider>
   );
 };
