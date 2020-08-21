6d2bebde813219136c2712248d9208e4fe2399b8
Elijah Lee <develijahlee@gmail.com>
2020-08-21 12:36:25 +0900

    change resume

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2ee92347d039016fca72460594b83960.js"
+  "/portfolio/precache-manifest.f9afbf698e5ee3bef4b5efe902521341.js"
 );
 
 self.addEventListener('message', (event) => {
